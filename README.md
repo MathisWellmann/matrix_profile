f91518f08262bed4a91d6acff3a87ae4cbd6b5c0
MathisWellmann <wellmannmathis@gmail.com>
2023-12-09 23:29:05 +0100

    Add function `index_with_most_similar_sequence` with test

---
+++ README.md
@@ -0,0 +1,4 @@
+# Distance Profile / Matrix Profile
+
+Note that `RUSTFLAGS="-C targe-cpu=native"` shows performance improvements of up to 25% for larger window sizes.
+Run `cargo bench` with and without the flag to see for yourself.
--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,42 @@ use simd_euclidean::Vectorized;
 #[cfg(test)]
 mod load_from_csv;
 
-/// A simd version of the distance profile.
+/// The `distance_profile` computes a euclidean distance from a sliding window of `history` to a reference `window`.
+///
+/// Note that the timeseries data should be stationary / be in the same value range.
+/// Otherwise you may bias the distance computation.
+/// This uses the squared euclidean distance.
+///
+/// The `window` is assumed to be non-overlapping with `history` and located at the end as such:
+/// | `history` | `window` |
+/// This ensures the trivial match of `window` == `window` is not returned nor any indices that are too close.
 ///
 /// # Arguments:
 /// `window`: The most recent sliding window which to compute a vector of euclidian distances agains.
 /// `history`: All the previous datapoints, including the `window` or not.
 ///
-/// # Returns:
-/// A vector of length `history.len()`.
 pub fn distance_profile(window: &[f32], history: &[f32]) -> Vec<f32> {
-    let mut out = vec![0.0; history.len()];
-    for i in 0..history.len() - window.len() {
+    Vec::from_iter((0..history.len() - window.len() + 1).map(|i| {
         let comp = &history[i..i + window.len()];
-        let dist = Vectorized::squared_distance(window, comp);
-        out[i + window.len() - 1] = dist;
-    }
-    out
+        Vectorized::squared_distance(window, comp)
+    }))
+}
+
+/// Find the starting index of the sequence which has the lowest euclidean distance to the specified `window`.
+/// The `window` is assumed to be non-overlapping with `history` and located at the end as such:
+/// | `history` | `window` |
+/// This ensures the trivial match of `window` == `window` is not returned nor any indices that are too close.
+pub fn index_with_most_similar_sequence(window: &[f32], history: &[f32]) -> Option<usize> {
+    let profile = distance_profile(window, history);
+
+    let min_idx = profile
+        .iter()
+        .enumerate()
+        .min_by(|(_, a), (_, b)| a.total_cmp(b))
+        .map(|(index, _)| index)?;
+    println!("min_idx: {min_idx}");
+
+    Some(min_idx)
 }
 
 #[cfg(test)]
@@ -34,14 +54,27 @@ mod tests {
 
     #[test]
     fn test_distance_profile() {
-        let a = Vec::<f32>::from_iter((0..10).map(|v| v as f32));
-        let window = &a[8..];
-        println!("a: {a:?}");
+        let history = Vec::<f32>::from_iter((0..10).map(|v| v as f32));
+        let window = &history[8..];
+        let history = &history[0..8];
+        println!("history: {history:?}");
         println!("window: {window:?}");
 
-        let profile = distance_profile(window, &a);
+        let profile = distance_profile(window, history);
         println!("profile: {profile:?}");
-        assert_eq!(profile.len(), a.len());
-        assert_eq!(profile[9], 0.0);
+        assert_eq!(&profile, &[128.0, 98.0, 72.0, 50.0, 32.0, 18.0, 8.0]);
+    }
+
+    #[test]
+    fn test_index_with_most_similar_sequence() {
+        let history = Vec::<f32>::from_iter((0..10).map(|v| v as f32));
+        let window = &history[8..];
+        let history = &history[0..8];
+        println!("history: {history:?}");
+        println!("window: {window:?}");
+
+        let idx = index_with_most_similar_sequence(window, history).expect("Is Some");
+        println!("idx: {idx}");
+        assert_eq!(idx, 6);
     }
 }
